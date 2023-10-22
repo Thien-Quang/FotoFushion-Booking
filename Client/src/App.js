53286abc23b15050d06ccc34fe9936fc4b36f6af
triminhle2002 <97251680+triminhle2002@users.noreply.github.com>
2023-10-22 17:54:52 +0700

    Add calendar page and Equipment page

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,23 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from "./components/Header/header"
-import Footer from './components/Footer/footer';
-import Home from './components/Home/home';
-
+import React from "react";
+import "./App.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import BookingPage from "./pages/BookingPage_v2";
+import AlbumPage from "./pages/AlbumPage";
+import EquipmentPage from "./pages/Equipment/EquipmentPage";
 
 function App() {
   return (
-    <Router>
-      <div className="h-screen min-w-[680px] flex flex-col">
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/home" element={<Home />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div style={{ background: "#000" }} className="py-5">
+      <EquipmentPage />
+    </div>
   );
 }
 
--- Server/server.js
+++ Server/server.js
@@ -1,25 +1,20 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-require('dotenv').config()
-const routes = require("./src/routes")
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+const routes = require("./src/routes");
 
-
-const port = process.env.PORT
-const hostname = process.env.HOST_NAME
+const port = process.env.PORT;
+const hostname = process.env.HOST_NAME;
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use(cors())
+app.use(cors());
 
-
-routes(app)
-
-
-
+routes(app);
 
 app.listen(port, hostname, () => {
-  console.log(`Server up and running on port ${port}`)
-})
+  console.log(`Server up and running on port ${port}`);
+});
